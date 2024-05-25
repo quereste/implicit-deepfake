99fc3a6e83bcd64cfcbdd2bbc18933aca9e84b7e
Stekoaa <kuba.steczkiewicz@wp.pl>
2024-05-25 11:23:42 +0200

    Cut videos and code cleanup

--- docs/static/js/index.js
+++ docs/static/js/index.js
@@ -1,24 +1,5 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
-var INTERP_BASE = "./static/interpolation/stacked";
-var NUM_INTERP_FRAMES = 240;
-
-var interp_images = [];
-function preloadInterpolationImages() {
-  for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
-    var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
-    interp_images[i] = new Image();
-    interp_images[i].src = path;
-  }
-}
-
-function setInterpolationImage(i) {
-  var image = interp_images[i];
-  image.ondragstart = function() { return false; };
-  image.oncontextmenu = function() { return false; };
-  $('#interpolation-image-wrapper').empty().append(image);
-}
-
 function downloadJSON(fileName) {
   const a = document.createElement('a');
   a.href = `./static/point_clouds/${fileName}`;
@@ -67,13 +48,7 @@ $(document).ready(function() {
         player.currentTime = player.duration / 100 * this.value;
       })
     }, false);*/
-    preloadInterpolationImages();
-
-    $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);
-    });
-    setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
+   
 
     bulmaSlider.attach();
     const menuItems = document.querySelectorAll('.menu-avatars-item') ?? [];
